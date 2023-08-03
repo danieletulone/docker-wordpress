8ca3f943083458117c2b10a0d71665a9f992ef14
danieletulone <danieletulone.work@gmail.com>
2023-08-03 12:51:19 +0200

    fix link command: add trigger

--- scripts/link:plugin.sh
+++ scripts/link:plugin.sh
@@ -1,2 +1,3 @@
 watchman watch $1
 rsync -zvrah $1 ./volumes/wordpress/plugins
+watchman -- trigger $1 docker-wordpress-plugin-$(basename $1)-trigger '*' -- sh -c "rsync -zvrah $1 ./volumes/wordpress/plugins --exclude node_modules"
--- scripts/link:theme.sh
+++ scripts/link:theme.sh
@@ -1,2 +1,4 @@
 watchman watch $1
-rsync -zvrah $1 ./volumes/wordpress/themes
+rsync -zvrah $1 ./volumes/wordpress/themes --exclude 'node_modules'
+watchman -- trigger $1 docker-wordpress-theme-$(basename $1)-trigger '*' -- sh -c "rsync -zvrah $1 ./volumes/wordpress/themes --exclude node_modules"
+
