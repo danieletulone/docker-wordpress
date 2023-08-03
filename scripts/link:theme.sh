09edc4277203be2a53c3e04df60c4f30fef301e8
danieletulone <danieletulone.work@gmail.com>
2023-08-03 13:00:57 +0200

    fix watchman trigger: use pwd because relative path will not work

--- scripts/link:theme.sh
+++ scripts/link:theme.sh
@@ -1,4 +1,4 @@
 watchman watch $1
 rsync -zvrah $1 ./volumes/wordpress/themes --exclude 'node_modules'
-watchman -- trigger $1 docker-wordpress-theme-$(basename $1)-trigger '*' -- sh -c "rsync -zvrah $1 ./volumes/wordpress/themes --exclude node_modules"
+watchman -- trigger $1 docker-wordpress-theme-$(basename $1)-trigger '**/*' -- sh -c "rsync -zvrah $1 $(pwd)/volumes/wordpress/themes --exclude node_modules"
 
